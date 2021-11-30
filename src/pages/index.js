ebf9166627c2ede8962b7f8604b14ccab03ad253
brett <brett.bartylla@outlook.com>
2021-11-30 15:07:18 -0600

    little updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Header 1</h1>
+    <p>Welcome to your new site.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
