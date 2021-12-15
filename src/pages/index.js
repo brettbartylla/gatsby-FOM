225ab9589bc0b42a91d220369a2931b7db7bbec1
brett <brett.bartylla@outlook.com>
2021-12-14 23:37:36 -0600

    feat(interaction): updated landing page images to webp, replaced videos with updated versions

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import Seo from "../components/seo"
 
 //images
 import Img from 'gatsby-image'
-import cardGif from '../images/cards.gif'
 import cardVid from '../video/cards_hover.mp4'
 import clothesVid from '../video/clothes_hover.mp4'
 
@@ -27,13 +26,11 @@ const IndexPage = ({data}) => (
             className="cardImg"
           />
           <div className="img-top">
-            <h2 className="u-posAbsolute">cards</h2>
+            <h1 className="u-posAbsolute">cards</h1>
             <video muted autoPlay loop id="cardVideo" width="250" aria-hidden="true">
               <source src={cardVid} type="video/mp4" />
             </video>
           </div>
-
-          {/* <img src={cardGif} alt="Logo" className="img-top"/> */}
         </div>
       </Link>
       </div>
@@ -49,16 +46,15 @@ const IndexPage = ({data}) => (
         <Link to="/cards/">
           <div className="imgFlip">
             <Img 
-              fluid={data.cardGatsbyImage.childImageSharp.fluid}
+              fluid={data.clothesGatsbyImage.childImageSharp.fluid}
               alt="Handmade cards"
               className="cardImg"
             />
             <div className="img-top">
-              <h2 className="u-posAbsolute">apparel</h2>
+              <h1 className="u-posAbsolute">apparel</h1>
               <video muted autoPlay loop id="apparelVideo" width="250" aria-hidden="true">
                 <source src={clothesVid} type="video/mp4" />
               </video>
-              {/* <img src={cardGif} alt="Logo" className="img-top"/> */}
             </div>
           </div>
         </Link>
@@ -74,7 +70,14 @@ export default IndexPage
 //image queries
 export const query = graphql`
   query {
-    cardGatsbyImage: file(relativePath: {eq: "cards.png"}) {
+    cardGatsbyImage: file(relativePath: {eq: "cards.webp"}) {
+      childImageSharp {
+        fluid(maxWidth: 2000) {
+        ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    clothesGatsbyImage: file(relativePath: {eq: "clothes.webp"}) {
       childImageSharp {
         fluid(maxWidth: 2000) {
         ...GatsbyImageSharpFluid
