2ee0be7cbbd67b96ef961560cb0424b744aab436
brett <brett.bartylla@outlook.com>
2022-02-11 16:16:01 -0600

    feat(accessibility): added aria labels to social media icon links

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,12 +71,12 @@ const IndexPage = ({data}) => (
     </p>
     <ul className="p-t-5 social-list u-textCenter m-r-20">
         <li className="u-inlineBlock">
-          <a href="https://www.instagram.com/byfriendsofmine/" target="_blank" rel="noreferrer">
+          <a href="https://www.instagram.com/byfriendsofmine/" target="_blank" rel="noreferrer" aria-label="Instagram">
             <FontAwesomeIcon icon={faInstagram} />
           </a>
         </li>
         <li className="u-inlineBlock">
-          <a href="mailto:by.friendsofmine@gmail.com" rel="noreferrer">
+          <a href="mailto:by.friendsofmine@gmail.com" rel="noreferrer" aria-label="Email">
             <FontAwesomeIcon icon={faEnvelopeOpen} />
           </a>
         </li>
