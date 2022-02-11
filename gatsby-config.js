b47a458943c8d574527bf67409f7b694494b1d29
brett <brett.bartylla@outlook.com>
2022-02-11 16:02:39 -0600

    feat(seo): added robots.txt plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
