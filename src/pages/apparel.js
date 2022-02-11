8ec8ca115852dddc70b764bc9421510b3df32941
brett <brett.bartylla@outlook.com>
2022-02-10 18:26:57 -0600

    refactor(products): updated apparel order

--- src/pages/apparel.js
+++ src/pages/apparel.js
@@ -13,20 +13,6 @@ const SecondPage = ({data}) => (
       <h1 className="ellipse">APPAREL</h1>
       <div className="Grid--products Products">
         <a 
-          href="https://www.etsy.com/listing/1114572567/bunny-bunny-handmade-tote-bag-no-waste?click_key=3c060c29852c0c51e8bfb70c038baf41c5372e51%3A1114572567&click_sum=c9ed6a4b&ref=shop_home_feat_2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="u-textCenter item">
-            <Img 
-                fluid={data.toteBagGatsbyImage.childImageSharp.fluid}
-                imgStyle={{ objectFit: 'contain' }}
-                alt="bunny bunny tote bag"
-            />
-            <p className="txt-bold p-t-8">Bunny Bunny Tote Bag</p>
-          </div>
-        </a>
-        <a 
           href="https://www.etsy.com/listing/1136564387/take-care-long-sleeve-heavy-cotton-shirt?click_key=f39b7c4fa13b0ca6aa67650175ebb7b83eb958c7%3A1136564387&click_sum=d14dcba2&ref=shop_home_feat_1&variation0=2387384391"
           target="_blank"
           rel="noreferrer"
@@ -38,6 +24,20 @@ const SecondPage = ({data}) => (
                 alt="take care long sleeve tee, green"
             />
             <p className="txt-bold p-t-8">Take Care Long Sleeve Tee, Green</p>
+          </div>
+        </a>
+        <a 
+          href="https://www.etsy.com/listing/1114572567/bunny-bunny-handmade-tote-bag-no-waste?click_key=3c060c29852c0c51e8bfb70c038baf41c5372e51%3A1114572567&click_sum=c9ed6a4b&ref=shop_home_feat_2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="u-textCenter item">
+            <Img 
+                fluid={data.toteBagGatsbyImage.childImageSharp.fluid}
+                imgStyle={{ objectFit: 'contain' }}
+                alt="bunny bunny tote bag"
+            />
+            <p className="txt-bold p-t-8">Bunny Bunny Tote Bag</p>
           </div>
         </a>
         <a 
