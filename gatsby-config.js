34b679ebb4b167aa7263fc7c10001766ac494413
brett <brett.bartylla@outlook.com>
2022-02-11 15:52:06 -0600

    refactor(config): removed unused js file path

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,12 +47,12 @@ module.exports = {
         icon: `src/images/fom-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: 'gatsby-plugin-load-script',
-      options: {
-        src: '/video.js', // Change to the script filename
-      },
-    },
+    // {
+    //   resolve: 'gatsby-plugin-load-script',
+    //   options: {
+    //     src: '/video.js', // Change to the script filename
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
